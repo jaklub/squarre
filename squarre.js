1ce0069e72beb19f6b78a9fd6a5373073f45e5f2
Jakob <jakob.lublin@gmail.com>
2014-05-04 23:56:50 +0200

    hepp

--- squarre.js
+++ squarre.js
@@ -1,7 +1,82 @@
-	var app = angular.module("squarre", ["firebase"]);
+	//var app = angular.module("squarre", ["ui.bootstrap","firebase"]);
+	//angular.module("squarre", ['ui.bootstrap',"firebase"]);
+	var app = angular.module("squarre", ['ui.bootstrap', "firebase"]);
+
 	function GamesController($scope, $firebase) {
-		var ref = new Firebase("https://squarre.firebaseio.com/games");
-		$scope.games = $firebase(ref);
+		var playersRef = new Firebase("https://squarre.firebaseio.com/players");
+		var gamesRef = new Firebase("https://squarre.firebaseio.com/games");
+
+
+		var auth = new FirebaseSimpleLogin(gamesRef, function(error, user) {
+			if (error) {
+				// an error occurred while attempting login
+				alert(error);
+			} else if (user) {
+				// user authenticated with Firebase
+				console.log('User ID: ' + user.id + ', Provider: ' + user.provider + ', e-mail: ' + user.email);
+			} else {
+				auth.login('google');
+			}
+		});
+
+		var auth = new FirebaseSimpleLogin(playersRef, function(error, user) {
+			if (error) {
+				// an error occurred while attempting login
+				alert(error);
+			} else if (user) {
+				// user authenticated with Firebase
+				console.log('User ID: ' + user.id + ', Provider: ' + user.provider + ', e-mail: ' + user.email);
+			} else {
+				auth.login('google');
+			}
+		});
+
+		// attempt to log the user in with your preferred authentication provider
+		//auth.logout();
+		$scope.players = $firebase(playersRef);
+		$scope.games = $firebase(gamesRef);
+
+		//$scope.points = [{"name":"0","value":"0"},{"name":"1","value":"1"}];
+		$scope.points = [0,1,2,3];
+		$scope.point = $scope.points[0];
+
+		//$scope.userInfo = 'User ID: ' + user.id + ', Provider: ' + user.provider + ', e-mail: ' + user.email;
+
+		
+
+
+		$scope.oneAtATime = true;
+		$scope.status = {
+			isFirstOpen: false,
+			isFirstDisabled: false
+		};
+
+		$scope.saveGame = function(e) {
+			var newDateTime = getDateTime();
+			var newId = -1;
+			var player1 = $scope.player1;
+			var player2 = $scope.player2;
+			var points1 = $scope.score1;
+			var points2 = $scope.score2;
+
+			var newGame = {
+				"id": newId,
+				"dateTime": newDateTime,
+				"players": [{
+					"id": player1,
+					"points": points1
+				}, {
+					"id": player2,
+					"points": points2
+				}]
+			};
+
+			console.log("saveGame(), player1: " + player1 + ", player2:" + player2);
+			console.log(console.log(JSON.stringify(newGame, null, 2)));
+
+			$scope.games.$add(newGame);
+
+		}
 
 	}
 
@@ -11,7 +86,7 @@
 		var gamesRef = new Firebase("https://squarre.firebaseio.com/games");
 		$scope.games = $firebase(gamesRef);
 
-		$scope.saveGame = function(e){
+		$scope.saveGame = function(e) {
 			var newDateTime = getDateTime();
 			var newId = -1;
 			var player1 = $scope.player1;
@@ -19,54 +94,50 @@
 			var points1 = $scope.score1;
 			var points2 = $scope.score2;
 
-			var newGame =
-				{
-					"id": newId,
-					"dateTime": newDateTime,
-					"players": [
-					{
-						"id": player1,
-						"points": points1
-					},
-					{
-						"id": player2,
-						"points": points2
-					}        
-					]
-				};
+			var newGame = {
+				"id": newId,
+				"dateTime": newDateTime,
+				"players": [{
+					"id": player1,
+					"points": points1
+				}, {
+					"id": player2,
+					"points": points2
+				}]
+			};
 
 			console.log("saveGame(), player1: " + player1 + ", player2:" + player2);
-			console.log(console.log(JSON.stringify(newGame,null,2)));
+			console.log(console.log(JSON.stringify(newGame, null, 2)));
 
 			$scope.games.$add(newGame);
 
 		}
-	}      
+	}
 
 
 	function getDateTime() {
-		var now     = new Date(); 
-		var year    = now.getFullYear();
-		var month   = now.getMonth()+1; 
-		var day     = now.getDate();
-		var hour    = now.getHours();
-		var minute  = now.getMinutes();
-		var second  = now.getSeconds(); 
-		if(month.toString().length == 1) {
-			var month = '0'+month;
+		var now = new Date();
+		var year = now.getFullYear();
+		var month = now.getMonth() + 1;
+		var day = now.getDate();
+		var hour = now.getHours();
+		var minute = now.getMinutes();
+		var second = now.getSeconds();
+		if (month.toString().length == 1) {
+			var month = '0' + month;
 		}
-		if(day.toString().length == 1) {
-			var day = '0'+day;
-		}   
-		if(hour.toString().length == 1) {
-			var hour = '0'+hour;
+		if (day.toString().length == 1) {
+			var day = '0' + day;
 		}
-		if(minute.toString().length == 1) {
-			var minute = '0'+minute;
+		if (hour.toString().length == 1) {
+			var hour = '0' + hour;
 		}
-		if(second.toString().length == 1) {
-			var second = '0'+second;
-		}   
-		var dateTime = year+'-'+month+'-'+day+' '+hour+':'+minute+':'+second;   
+		if (minute.toString().length == 1) {
+			var minute = '0' + minute;
+		}
+		if (second.toString().length == 1) {
+			var second = '0' + second;
+		}
+		var dateTime = year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second;
 		return dateTime;
 	}
