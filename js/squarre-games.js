c628641a2e0e31f0433189805a459440471a0077
Jakob <jakob.lublin@gmail.com>
2014-05-17 01:05:46 +0200

    asds

--- js/squarre-games.js
+++ js/squarre-games.js
@@ -2,7 +2,7 @@
 	//var app = angular.module("squarre", ["ui.bootstrap","firebase"]);
 	//angular.module("squarre", ['ui.bootstrap',"firebase"]);
 	// var app = angular.module("squarre", ['ui.bootstrap', "firebase"])
-	var squarre = angular.module("squarre", ['firebase', 'infinite-scroll']);
+	var squarre = angular.module("squarre", ['firebase', 'infinite-scroll','ngAnimate']);
 	squarre.factory("GamesService", ["$firebase", "$rootScope", "$timeout",  function($firebase, $rootScope, $timeout) {
 			var gamesRef = new Firebase("https://squarre.firebaseio.com/games");
 			var gamesWithLimitRef = new Firebase("https://squarre.firebaseio.com/games");
@@ -44,6 +44,12 @@
 				$scope.gamesWithLimit = service.getGamesWithLimit($scope.gamesLimit);
 				$scope.games = service.getGames();
 				$scope.pointsArray = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];				
+				$scope.gameUpdateId ="";
+
+					$scope.updatePlayer1Id = "";
+					$scope.updatePlayer2Id = "";
+					$scope.updatePlayer1Points = "";
+					$scope.updatePlayer2Points = "";
 
 				$scope.saveGame = function(e) {
 					var newDateTime = getDateTime();
@@ -79,8 +85,48 @@
 					$scope.gamesLimit = $scope.gamesLimit + 5;		
 					$scope.gamesWithLimit = service.getGamesWithLimit($scope.gamesLimit);
 				}
-				$scope.editGame  = function(e) {
+				$scope.updateGame  = function(gameId) {
 					var temp = "";
+					if($scope.gameUpdateId === gameId){
+						$scope.gameUpdateId = "";
+					}
+					else{
+						$scope.gameUpdateId = gameId;
+
+						var updateGame = $scope.games[gameId];
+						$scope.$parent.updatePlayer1Id = updateGame.players[0].id;
+						$scope.$parent.updatePlayer2Id = updateGame.players[1].id;
+						$scope.$parent.updatePlayer1Points = updateGame.players[0].points;
+						$scope.$parent.updatePlayer2Points = updateGame.players[1].points;
+					}
+
+
+					var temp = "";
+
+				}
+				$scope.saveUpdateGame  = function(gameId) {
+					var updateGame = $scope.games.$child(gameId);
+					var updatePlayers = updateGame.$child("players");
+					var player1 = $scope.$parent.updatePlayer1Id;
+					var player2 = $scope.$parent.updatePlayer2Id;
+					var points1 = $scope.$parent.updatePlayer1Points;
+					var points2 = $scope.$parent.updatePlayer2Points;					
+
+					var newPlayers = [{
+								"id": player1,
+								"points": points1
+							}, {
+								"id": player2,
+								"points": points2
+							}]
+						
+					updatePlayers.$set(newPlayers);
+
+					var temp = "";
+					$scope.gameUpdateId ="";
+				}
+				$scope.cancelUpdateGame  = function(gameId) {
+					$scope.gameUpdateId = "";
 				}
 				$scope.evalWinnerCSS = function(playerPoints, oponentPoints){
 					if(playerPoints === oponentPoints){
